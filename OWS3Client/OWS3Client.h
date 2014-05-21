48057065fdb6f0f4c3dfaf5983692a150d8a9800
Chris Ballinger <chrisballinger@gmail.com>
2014-05-21 13:52:11 -0700

    Add support for Cache-Control header

--- OWS3Client/OWS3Client.h
+++ OWS3Client/OWS3Client.h
@@ -10,15 +10,46 @@
 #import "AmazonEndpoints.h"
 #import "S3/AmazonS3Client.h"
 
+/**
+ *  OWS3Client, an easier way to upload files and data to S3.
+ */
 @interface OWS3Client : NSObject
 
+/**
+ *  Internal S3 client.
+ */
 @property (nonatomic, strong) AmazonS3Client *s3;
-@property (nonatomic) dispatch_queue_t networkQueue; // defaults to DISPATCH_QUEUE_PRIORITY_DEFAULT
-@property (nonatomic) dispatch_queue_t callbackQueue; //defaults to dispatch_get_main_queue
-@property (nonatomic) AmazonRegion region; //defaults to US East 1
-@property (nonatomic) BOOL useSSL; //defaults to YES
 
-- (id) initWithAccessKey:(NSString*)accessKey secretKey:(NSString*)secretKey;
+/**
+ *  defaults to DISPATCH_QUEUE_PRIORITY_DEFAULT
+ */
+@property (nonatomic) dispatch_queue_t networkQueue;
+
+/**
+ *  defaults to dispatch_get_main_queue
+ */
+@property (nonatomic) dispatch_queue_t callbackQueue;
+
+/**
+ *  defaults to US East 1
+ */
+@property (nonatomic) AmazonRegion region;
+
+/**
+ *  defaults to YES
+ */
+@property (nonatomic) BOOL useSSL;
+
+/**
+ *  Do not bundle AWS secret keys with your application.
+ *  http://mobile.awsblog.com/post/Tx31X75XISXHRH8/Managing-Credentials-in-Mobile-Applications
+ *
+ *  @param accessKey AWS access key
+ *  @param secretKey AWS secret key
+ *
+ *  @return OWS3Client
+ */
+- (instancetype) initWithAccessKey:(NSString*)accessKey secretKey:(NSString*)secretKey;
 
 /**
  Adds an object to a bucket using forms.
@@ -40,6 +71,25 @@
 /**
  Adds an object to a bucket using forms.
  
+ @param path The path to the local file.
+ @param bucket The destination bucket.
+ @param key Optional. Defaults to the last path component of `path`.
+ @param acl Optional. If unset it uses the global value for acl which defaults to public-read. You probably want either public-read or private. For more info check out S3CannedACL.h in aws-sdk-ios.
+ @param cacheControl Optional. Can be useful for frequently-changing objects behind Cloudfront.
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and takes a single argument: the response object from the server.
+ @param failure A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes a single argument: the `NSError` object describing error that occurred.
+ */
+- (void)postObjectWithFile:(NSString *)path
+                    bucket:(NSString*)bucket
+                       key:(NSString *)key
+                       acl:(NSString*)acl
+              cacheControl:(NSString*)cacheControl
+                   success:(void (^)(S3PutObjectResponse *responseObject))success
+                   failure:(void (^)(NSError *error))failure;
+
+/**
+ Adds an object to a bucket using forms.
+ 
  @param data The data you want to upload.
  @param bucket The destination bucket.
  @param key Optional. Defaults to the last path component of `path`.
@@ -51,6 +101,25 @@
                     bucket:(NSString*)bucket
                        key:(NSString *)key
                        acl:(NSString*)acl
+                   success:(void (^)(S3PutObjectResponse *responseObject))success
+                   failure:(void (^)(NSError *error))failure;
+
+/**
+ Adds an object to a bucket using forms.
+ 
+ @param data The data you want to upload.
+ @param bucket The destination bucket.
+ @param key Optional. Defaults to the last path component of `path`.
+ @param acl Optional. If unset it uses the global value for acl which defaults to public-read. You probably want either public-read or private. For more info check out S3CannedACL.h in aws-sdk-ios.
+ @param cacheControl Optional. Can be useful for frequently-changing objects behind Cloudfront.
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and takes a single argument: the response object from the server.
+ @param failure A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes a single argument: the `NSError` object describing error that occurred.
+ */
+- (void)postObjectWithData:(NSData *)data
+                    bucket:(NSString*)bucket
+                       key:(NSString *)key
+                       acl:(NSString*)acl
+              cacheControl:(NSString*)cacheControl
                    success:(void (^)(S3PutObjectResponse *responseObject))success
                    failure:(void (^)(NSError *error))failure;
 @end
--- OWS3Client/OWS3Client.m
+++ OWS3Client/OWS3Client.m
@@ -9,7 +9,6 @@
 #import "OWS3Client.h"
 
 @implementation OWS3Client
-@synthesize s3, networkQueue, callbackQueue, region, useSSL;
 
 - (id) initWithAccessKey:(NSString*)accessKey secretKey:(NSString*)secretKey {
     if (self = [super init]) {
@@ -23,8 +22,8 @@
 #endif
         [AmazonErrorHandler shouldNotThrowExceptions];
         self.s3 = [[AmazonS3Client alloc] initWithAccessKey:accessKey withSecretKey:secretKey];
-        region = US_EAST_1;
-        useSSL = YES;
+        _region = US_EAST_1;
+        _useSSL = YES;
         [self refreshEndpoint];
         self.networkQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
         self.callbackQueue = dispatch_get_main_queue();
@@ -36,83 +35,87 @@
     self.s3.endpoint = [AmazonEndpoints s3Endpoint:self.region secure:self.useSSL];
 }
 
-- (void) setRegion:(AmazonRegion)_region {
-    region = _region;
+- (void) setRegion:(AmazonRegion)region {
+    _region = region;
     [self refreshEndpoint];
 }
 
-- (void) setUseSSL:(BOOL)_useSSL {
-    useSSL = _useSSL;
+- (void) setUseSSL:(BOOL)useSSL {
+    _useSSL = useSSL;
     [self refreshEndpoint];
 }
 
-- (void) postObjectWithData:(NSData *)data bucket:(NSString *)bucket key:(NSString *)key acl:(NSString *)acl success:(void (^)(S3PutObjectResponse *))success failure:(void (^)(NSError *))failure {
-    dispatch_async(networkQueue, ^{
-        // Upload image data.  Remember to set the content type.
-        S3PutObjectRequest *por = [[S3PutObjectRequest alloc] initWithKey:key inBucket:bucket];
-        por.data  = data;
-        
-        S3CannedACL *cannedACL = nil;
-        if (acl) {
-            cannedACL = [[S3CannedACL alloc] initWithStringValue:acl];
-        } else {
-            cannedACL = [S3CannedACL publicRead];
-        }
-        por.cannedACL = cannedACL;
-        
-        // Put the image data into the specified s3 bucket and object.
-        S3PutObjectResponse *putObjectResponse = [self.s3 putObject:por];
-        
-        if (putObjectResponse.error) {
-            if (failure) {
-                dispatch_async(callbackQueue, ^{
-                    failure(putObjectResponse.error);
-                });
-            }
-            return;
-        }
-        
-        if (success) {
-            dispatch_async(callbackQueue, ^{
-                success(putObjectResponse);
-            });
-        }
-    });
+- (void) postObjectWithData:(NSData *)data
+                     bucket:(NSString *)bucket
+                        key:(NSString *)key
+                        acl:(NSString *)acl
+               cacheControl:(NSString *)cacheControl
+                    success:(void (^)(S3PutObjectResponse *))success
+                    failure:(void (^)(NSError *))failure {
+    S3PutObjectRequest *por = [[S3PutObjectRequest alloc] initWithKey:key inBucket:bucket];
+    por.data  = data;
+    [self putS3ObjectRequest:por acl:acl cacheControl:cacheControl success:success failure:failure];
+}
+
+- (void) postObjectWithData:(NSData *)data
+                     bucket:(NSString *)bucket
+                        key:(NSString *)key
+                        acl:(NSString *)acl
+                    success:(void (^)(S3PutObjectResponse *))success
+                    failure:(void (^)(NSError *))failure {
+    [self postObjectWithData:data bucket:bucket key:key acl:acl cacheControl:nil success:success failure:failure];
 }
 
 - (void)postObjectWithFile:(NSString *)path
                     bucket:(NSString*)bucket
                        key:(NSString *)key
                        acl:(NSString*)acl
+              cacheControl:(NSString *)cacheControl
                    success:(void (^)(S3PutObjectResponse *responseObject))success
                    failure:(void (^)(NSError *error))failure {
-    dispatch_async(networkQueue, ^{
-        // Upload image data.  Remember to set the content type.
-        S3PutObjectRequest *por = [[S3PutObjectRequest alloc] initWithKey:key inBucket:bucket];
-        por.filename  = path;
-        
+    S3PutObjectRequest *por = [[S3PutObjectRequest alloc] initWithKey:key inBucket:bucket];
+    por.filename  = path;
+    [self putS3ObjectRequest:por acl:acl cacheControl:cacheControl success:success failure:failure];
+}
+
+- (void) postObjectWithFile:(NSString *)path
+                     bucket:(NSString *)bucket
+                        key:(NSString *)key
+                        acl:(NSString *)acl
+                    success:(void (^)(S3PutObjectResponse *))success
+                    failure:(void (^)(NSError *))failure {
+    [self postObjectWithFile:path bucket:bucket key:key acl:acl cacheControl:nil success:success failure:failure];
+}
+
+- (void) putS3ObjectRequest:(S3PutObjectRequest*)request
+                        acl:(NSString*)acl
+               cacheControl:(NSString *)cacheControl
+                    success:(void (^)(S3PutObjectResponse *responseObject))success
+                    failure:(void (^)(NSError *error))failure {
+    dispatch_async(_networkQueue, ^{
         S3CannedACL *cannedACL = nil;
         if (acl) {
             cannedACL = [[S3CannedACL alloc] initWithStringValue:acl];
         } else {
             cannedACL = [S3CannedACL publicRead];
         }
-        por.cannedACL = cannedACL;
+        request.cannedACL = cannedACL;
+        request.cacheControl = cacheControl;
         
         // Put the image data into the specified s3 bucket and object.
-        S3PutObjectResponse *putObjectResponse = [self.s3 putObject:por];
+        S3PutObjectResponse *putObjectResponse = [self.s3 putObject:request];
         
         if (putObjectResponse.error) {
             if (failure) {
-                dispatch_async(callbackQueue, ^{
+                dispatch_async(_callbackQueue, ^{
                     failure(putObjectResponse.error);
                 });
             }
             return;
         }
         
-        if (success) {
-            dispatch_async(callbackQueue, ^{
+        if (success && putObjectResponse) {
+            dispatch_async(_callbackQueue, ^{
                 success(putObjectResponse);
             });
         }
